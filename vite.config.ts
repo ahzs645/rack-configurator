208ce4fe04bd331d19741f8c2f491a750712d53e
Ahmad Jalil <ahzs645@gmail.com>
2026-01-22 17:28:11 -0800

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/rack-configurator/' : '/',
   plugins: [react()],
   worker: {
     format: 'iife', // Use classic worker format instead of ES modules
